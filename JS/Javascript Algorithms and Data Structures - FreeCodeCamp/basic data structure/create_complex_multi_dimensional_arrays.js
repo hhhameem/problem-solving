944854844f4a8e89d13ddcabead75eae5abbdfd5
hhhameem <hebronhamim@gmail.com>
2021-09-13 19:54:22 +0600

    Complete Basic Data Structures of freecodecamp JS algo and ds

---
+++ JS/Javascript Algorithms and Data Structures - FreeCodeCamp/basic data structure/create_complex_multi_dimensional_arrays.js
@@ -0,0 +1,18 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['level two'],
+  [
+    ['deep'], ['level three'] 
+  ],
+  [
+    [
+      ['deeper'], ['level four']
+    ],
+    [
+      [
+        ['deepest'], ['level five']
+      ]
+    ]
+  ]
+  // Only change code above this line
+];
