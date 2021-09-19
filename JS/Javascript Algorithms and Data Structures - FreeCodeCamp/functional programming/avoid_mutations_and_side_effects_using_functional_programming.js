86dd31dce2b20b163e8841f9a6fe9d097503d4d9
hhhameem <hebronhamim@gmail.com>
2021-09-19 22:00:59 +0600

    first 5 challenges from functional programming part of freecodecamp JS algo and ds

---
+++ JS/Javascript Algorithms and Data Structures - FreeCodeCamp/functional programming/avoid_mutations_and_side_effects_using_functional_programming.js
@@ -0,0 +1,10 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+  let changedValue = fixedValue;
+  changedValue+=1;
+  return changedValue;
+  // Only change code above this line
+}
